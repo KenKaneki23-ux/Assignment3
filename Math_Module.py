c086bfeb0fd22ecf28f92b1e40fd642e542bd79d
Ken <tushar260104@gmail.com>
2025-05-06 22:23:17 +0530

    Add files via upload

---
+++ Math_Module.py
@@ -0,0 +1,8 @@
+import math
+from math import *
+
+n=float(input('Enter your number: '))
+print(f"Square of {n} is: ",n * n)
+print(f"Natural Logarithm of {n} is: ",math.log(n, e))
+radians = n / 2
+print(f"Sine of the {n} is: ",math.sin(radians))
